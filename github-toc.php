5f4309e86065d801ab65acfdbeb4dc1d4d70ae29
xingshaocheng <xingshaocheng@users.noreply.github.com>
2018-04-22 10:36:26 +0800

    Update github-toc.php

--- github-toc.php
+++ github-toc.php
@@ -1,33 +1,46 @@
 <?php
+/**
+ * site: https://github.com/xingshaocheng/simple-php-github-toc 
+ */
 
 if(count($argv) < 2){
-	exit("Please input github file url, eg 'php github-toc.php https://github.com/xingshaocheng/architect-awesome/blob/master/README.md'.\n");
+        exit("Please input github file url, eg 'php github-toc.php https://github.com/xingshaocheng/architect-awesome/blob/master/README.md'.\n");
 }
 
 $url = $argv[1]; 
 
-$target_url = str_replace("github.com","raw.githubusercontent.com",$url);
-$target_url = str_replace("/blob/","/",$target_url);
-$content = file_get_contents($target_url);
+#$url = "https://github.com/xingshaocheng/architect-awesome/blob/master/README.md";
 
-$lines = explode("\n",$content);
 
-foreach($lines as $line){
-	$line = trim($line);
-
-	if(0 !== strpos($line, "#")){
-		continue;
-	}
-
-	$level = substr_count($line,"#");
-
-	$title = substr($line,$level);
-	$title = trim($title);
-	$title = str_replace('*','\*',$title);
-
-	$each_url = $url."#".urlencode($title);
-
-	echo str_repeat("\t",($level - 1)),"* ","[${title}](${each_url})\n";
-
+function get_anchor($content){
+        preg_match_all("/href=\"(.*)\">/iUs",  $content, $anchor_arr);
+        if(count($anchor_arr) > 0){
+                return $anchor_arr[1][0];
+        }
+        return "";
 }
 
+function get_title($content){
+        preg_match_all("/a\>(.*)$/iUs",  $content, $title_arr);
+        if(count($title_arr) > 0){
+                return trim($title_arr[1][0]);
+        }
+        return "";
+}
+
+$content = file_get_contents($url);
+
+preg_match_all("/<article(.*)<\/article>/iUs", $content, $article);
+
+$article_html = $article[0][0];
+
+preg_match_all("/<h([1-6])>(.*)<\/h[1-6]{1}>/iUs",  $article_html, $each_head); 
+
+$len = count($each_head[0]);
+for($i = 0;$i < $len; $i++){
+        $level = $each_head[1][$i];
+        $each_content = $each_head[2][$i];
+        $anchor = get_anchor($each_content);
+        $title = get_title($each_content);
+        echo str_repeat("\t",($level-1)),"* ","[${title}]($url.${anchor})\n";
+}
